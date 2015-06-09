51ab54ec76e4d049312af2b9ef7a38d37b98b742
rajeswarakumar <rajeswarakumar@users.noreply.github.com>
2015-06-09 16:40:15 +0530

    Create sample.server.routes.js

---
+++ app/routes/sample.server.routes.js
@@ -0,0 +1,14 @@
+module.exports = function(app) {
+
+        // server routes ===========================================================
+        // route to handle authentication routes goes here
+        // route to handle creating goes here (app.post)
+        // route to handle delete goes here (app.delete)
+
+        // frontend routes =========================================================
+        // route to handle all angular requests
+         app.get('*', function(req, res) {
+            res.sendfile('./Index.html'); // load our public/index.html file
+        });
+
+    };
